79ff6cc98dd522d37dae3136e94f67f61b25f391
MangoGab <gabrielhmango@gmail.com>
2023-07-13 20:21:32 -0300

    Criação da função preprocessamento e testes

--- app/processing.py
+++ app/processing.py
@@ -14,28 +14,58 @@ paragrafos = dados_html.find_all('p')
 
 conteudo = ''
 for p in paragrafos:
-  conteudo += p.text
+    conteudo += p.text
 
 lista_sentencas = nltk.sent_tokenize(conteudo)
 
 pln = spacy.load('pt_core_news_sm')
 stop_words = spacy.lang.pt.stop_words.STOP_WORDS
 
+def preprocessamento(texto):
+    texto = re.sub(r"https?://[A-Za-z0-9./]+", ' ', texto)
+    texto = re.sub(r" +", ' ', texto)
+
+    documento = pln(texto)
+    lista = []
+    for token in documento:
+       lista.append(token.lemma_)
+    
+    lista = [palavra for palavra in lista if palavra not in stop_words and palavra not in string.punctuation]
+    lista = ' '.join([str(elemento) for elemento in lista if not elemento.isdigit()])
+
+    return lista
+
+lista_sentencas_preprocessada = []
+for i in range(len(lista_sentencas)):
+    lista_sentencas_preprocessada.append(preprocessamento(lista_sentencas[i]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     os.system('cls')
 
-    # print(dados)
-    # print(dados_html)
+    # texto_teste = 'https://www.google.com.br/' + lista_sentencas[0]
+    # print(texto_teste)
 
-    # print(paragrafos)
-    # print(len(paragrafos))
+    # resultado = preprocessamento(texto_teste)
+    # print(resultado)
 
-    # print(conteudo)
-    # print(len(conteudo))
-
-    # print(lista_sentencas)
-    # print(len(lista_sentencas))
-
-    # print(pln)
-    # print(stop_words)
-    # print(len(stop_words))
+    for _ in range(5):
+        i = random.randint(0, len(lista_sentencas) - 1)
+        print(lista_sentencas[i])
+        print()
+        print(lista_sentencas_preprocessada[i])
+        print('-----' * 5)
